de64907a018454f33b0fe633ec6d965076aa36f2
Carlos Edmundo Martinez Mendoza <contacto@cedmundomartinez.com>
2014-02-24 11:34:25 -0600

    Cambios menores para reducir las advertencias

--- Practica 2/postfixy.c
+++ Practica 2/postfixy.c
@@ -1,37 +1,41 @@
-/*
-Copyright (c) 2014, Carlos Edmundo Martínez Mendoza 
-All rights reserved. 
 
-Redistribution and use in source and binary forms, with or without 
-modification, are permitted provided that the following conditions are met: 
+// Copyright (c) 2014, Carlos Edmundo Martínez Mendoza 
+// All rights reserved. 
 
- * Redistributions of source code must retain the above copyright notice, 
-   this list of conditions and the following disclaimer. 
- * Redistributions in binary form must reproduce the above copyright 
-   notice, this list of conditions and the following disclaimer in the 
-   documentation and/or other materials provided with the distribution. 
- * Neither the name of  nor the names of its contributors may be used to 
-   endorse or promote products derived from this software without specific 
-   prior written permission. 
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions are met: 
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
-ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
-LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
-CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
-SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
-INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
-ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
-POSSIBILITY OF SUCH DAMAGE. 
- */
+//  * Redistributions of source code must retain the above copyright notice, 
+//    this list of conditions and the following disclaimer. 
+//  * Redistributions in binary form must reproduce the above copyright 
+//    notice, this list of conditions and the following disclaimer in the 
+//    documentation and/or other materials provided with the distribution. 
+//  * Neither the name of  nor the names of its contributors may be used to 
+//    endorse or promote products derived from this software without specific 
+//    prior written permission. 
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+// POSSIBILITY OF SUCH DAMAGE. 
+ 
 
 #include "postfixy.h"
 #include "stacky.h"
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 int8_t perform_postfixed (StackType * buffer, Stack * primary, Stack * auxiliar) {
-    int i;
+    int32_t i;
     StackType cur;
     
     for (i = 0; i < strlen(buffer); i++ ){
@@ -72,7 +76,7 @@ int8_t perform_postfixed (StackType * buffer, Stack * primary, Stack * auxiliar)
 }
 
 int8_t get_precedence(StackType op){
-    int prec;
+    int8_t prec = -1;
     
     switch(op){
         case '^': prec = 2; break;
@@ -98,8 +102,8 @@ int8_t is_variable (StackType var){
 }
 
 int8_t is_high_than_top (Stack * s, StackType op){
-    int precedence_oper = get_precedence(op);
-    int precedence_top = get_precedence(check_top(s));
+    int8_t precedence_oper = get_precedence(op);
+    int8_t precedence_top = get_precedence(check_top(s));
     //printf ("%c > %c? %d\n", op, check_top(s), precedence_oper > precedence_top);
     
     return precedence_oper > precedence_top;
