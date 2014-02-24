680e4ae33e61cfdace3a859b909268ede72454f4
Carlos Edmundo Martínez Mendoza <cmartinez@metaservicios.com>
2014-02-24 15:35:59 +0000

    Cambio de comportamiento: si la cadena es inválida el programa no se interrumpe

--- static-stack/pilas_1cv1.c
+++ static-stack/pilas_1cv1.c
@@ -1,3 +1,40 @@
+/*
+Copyright (c) 2014, Carlos Edmundo Martínez Mendoza 
+All rights reserved. 
+
+Redistribution and use in source and binary forms, with or without 
+modification, are permitted provided that the following conditions are met: 
+
+ * Redistributions of source code must retain the above copyright notice, 
+   this list of conditions and the following disclaimer. 
+ * Redistributions in binary form must reproduce the above copyright 
+   notice, this list of conditions and the following disclaimer in the 
+   documentation and/or other materials provided with the distribution. 
+ * Neither the name of  nor the names of its contributors may be used to 
+   endorse or promote products derived from this software without specific 
+   prior written permission. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE 
+LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+POSSIBILITY OF SUCH DAMAGE. 
+ */
+
+/*
+* Programa para convertir de notación infoja a postfija.
+*
+* Alumno: Carlos Edmundo Martínez Mendoza.
+* Boleta: 2013630284.
+* Grupo: 1CV1.
+*/
+
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
@@ -13,7 +50,7 @@ typedef struct _Stack {
     int32_t top;
 } Stack;
 
-void perform_postfixed (char * buffer, Stack * primary, Stack * axuliar);
+int8_t perform_postfixed (char * buffer, Stack * primary, Stack * axuliar);
 
 void init_stack(Stack * s);
 void push (Stack * s, StackType value);
@@ -42,18 +79,23 @@ int main(int argc, char **argv){
     printf("Ingrese una operación en notación normal (no más de %d caracteres): ", STATIC_SIZE);
     
     do {
-        scanf("%s", buffer);
+        do {
+            scanf("%s", buffer);
         
-        if (strlen(buffer) > STATIC_SIZE - 1){
-            printf("La cadena debe tener una longitud menor o igual a quince caracteres.\n");
-        }else{
-            break;
-        }
-    } while (1);
+            if (strlen(buffer) > STATIC_SIZE - 1){
+                 printf("La cadena debe tener una longitud menor o igual a quince caracteres.\n");
+            }else{
+                 break;
+            }
+        } while (1);
     
-    perform_postfixed (buffer, &primary, &auxiliar);
+        if (perform_postfixed (buffer, &primary, &auxiliar)){
+	    printf("No se ha ingresado una entrada válida\n");
+    }else break;
+
+    } while(1);
     
-    printf("\nLa pila: ");
+    printf("Resultado en notación postfija: ");
     print_stack (&primary);
     
     printf("\n");
@@ -83,33 +125,25 @@ void init_stack (Stack * s){
     s->top = -1;
 }
 
-void perform_postfixed (char * buffer, Stack * primary, Stack * auxiliar) {
+int8_t perform_postfixed (char * buffer, Stack * primary, Stack * auxiliar) {
     int i;
     char cur;
     
     for (i = 0; i < strlen(buffer); i++ ){
         cur = toupper(buffer[i]);
-        //printf ("-------------------------------------\nEvaluando: %c\n", cur);
         
         if ( is_variable(cur) ){
-            //printf ("Es variable, ingresando directamente\n");
             push (primary, cur);
             
         }else if ( is_supported_operator (cur) ){
-            //printf ("Es un operador.\n");
             
             if (is_empty (auxiliar)){
-                //printf ("Auxiliar esta vacia, insertando en auxiliar.\n");
                 push (auxiliar, cur);
-                //print_stack(auxiliar);
-                //printf ("\n");
             }else{
                 if (is_high_than_top(auxiliar, cur)){
-                    //printf ("Tiene mayor precedencia, apliando en auxiliar\n");
                     push (auxiliar, cur);
                     
                 } else {
-                    //printf ("No tiene mayor precedencia.\n");
                     
                     while (  !is_high_than_top(auxiliar, cur) && !is_empty(auxiliar)){
                         push (primary, pop(auxiliar));
@@ -120,20 +154,22 @@ void perform_postfixed (char * buffer, Stack * primary, Stack * auxiliar) {
             }
             
         }else{
-            //printf("No se ha ingresado un caracter válido.\n");
-            exit(0);
+            printf("No se ha ingresado un caracter válido.\n");
+            return 1;
         }
     }
     
     while (!is_empty(auxiliar)) {
         push(primary, pop(auxiliar));
     }
+
+    return 0;
 }
 
 void print_stack (Stack * s){
     int i;
     for (i=0;i<s->top+1;i++){
-        printf("|%c", s->contents[i]);
+        printf("%c", s->contents[i]);
     }
 }
 
