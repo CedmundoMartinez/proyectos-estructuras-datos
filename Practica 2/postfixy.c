4d58a3786903daa56e5d2b3bbc88d6388f1a3b98
Carlos Edmundo Martinez Mendoza <contacto@cedmundomartinez.com>
2014-03-03 12:48:39 -0600

    Intento 2; Correcciones menores para el funcionamiento (15)

--- Practica 2/postfixy.c
+++ Practica 2/postfixy.c
@@ -47,9 +47,11 @@ int8_t perform_postfixed (StackType * buffer, Stack * primary, Stack * auxiliar)
         
         if ( is_variable(cur) ){
             push (primary, cur);
+        }else if ( increases_prioriy(cur) ) {
+            push (auxiliar, cur);
         }else if ( decreases_prioriy(cur) ) {
             delete_higher_parenthesis (primary, auxiliar);
-        }else if ( is_supported_operator (cur) || increases_prioriy(cur) ){
+        }else if ( is_supported_operator (cur) ){
             
             if (is_empty (auxiliar)){
                 push (auxiliar, cur);
