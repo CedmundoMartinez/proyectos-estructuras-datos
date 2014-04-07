335e100c2af10d3803f9b3543c0e23403f4ec317
Carlos Edmundo Martinez Mendoza <contacto@cedmundomartinez.com>
2014-04-07 12:02:13 -0500

    Corrección de nombre en contacts.c

--- Practica 4/contacts.c
+++ Practica 4/contacts.c
@@ -46,8 +46,6 @@ int8_t read_string_field ( char ** field, FILE * stream ){
 
     while ( !(feof(stream) || ferror(stream)) ){
         c = fgetc(stream);
-
-        printf("%c", c);
         
         if (c == '\n' || c == ' ' || c == ';' || c == '|'){
             end_pos = ftell(stream);
@@ -57,12 +55,12 @@ int8_t read_string_field ( char ** field, FILE * stream ){
             if ( *field == NULL )
                 return 2;
 
-            fseek(stream, start_pos, SEEK_CUR);
+            fseek(stream, start_pos, SEEK_SET);
 
             if (fgets(*field, tok_size, stream) != *field)
                 return 0;
 
-            fseek(stream, end_pos + 1, SEEK_CUR);
+            fseek(stream, end_pos + 1, SEEK_SET);
             return 1;
         }
     }
